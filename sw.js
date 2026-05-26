fbab18808e92232e4e753ada6f54dea13de57705
Vyacheslaw Munister <vyacheslawmunister@MacBook-Air-Vyacheslaw.local>
2026-05-26 23:25:27 +0300

    Add explicit board clipboard paste action

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'teachedos-v55';
+const CACHE = 'teachedos-v56';
 const BASE_PATH = new URL(self.registration.scope).pathname;
 const base = path => new URL(path, self.registration.scope).pathname;
 
