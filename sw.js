9e5f819ad6d1d2b7a546112d731aee26a23d9b47
Vyacheslaw Munister <vyacheslawmunister@MacBook-Air-Vyacheslaw.local>
2026-05-26 16:53:54 +0300

    Add board comment threads

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'teachedos-v47';
+const CACHE = 'teachedos-v48';
 const BASE_PATH = new URL(self.registration.scope).pathname;
 const base = path => new URL(path, self.registration.scope).pathname;
 
