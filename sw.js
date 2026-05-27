6fdc209816aff98e254a2bf2e7762915a27680b8
Vyacheslaw Munister <vyacheslawmunister@MacBook-Air-Vyacheslaw.local>
2026-05-27 10:30:24 +0300

    Polish pointer tool and add AI assistant plan

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'teachedos-v59';
+const CACHE = 'teachedos-v60';
 const BASE_PATH = new URL(self.registration.scope).pathname;
 const base = path => new URL(path, self.registration.scope).pathname;
 
